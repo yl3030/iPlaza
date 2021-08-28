a6e9f941ed10ffa91a9fc06b4fbdf2f89d1adbfe
yl301114 <yl301114@gmail.com>
2021-08-29 03:02:33 +0800

    modify1

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,20 @@
-$(".check-box").click(function(){
-    $(this).toggleClass("active");
-})
-
 $(".menu").click(function(){
     $(this).toggleClass("active");
     $(".main-nav").toggleClass("active");
+})
+
+$("*").each(function () { 
+    $(this).click(function () { 
+       var target=$(this).attr("data-target");
+        if(target!=undefined){
+           var top=$(target).offset().top - 80;
+
+           $("html,body").animate({scrollTop:top},parseInt(300));
+        }
+    });
+});
+
+$(".check-box").click(function(){
+    $(".check-box").children(".radio-box").removeClass("active")
+    $(this).children(".radio-box").addClass("active");
 })
